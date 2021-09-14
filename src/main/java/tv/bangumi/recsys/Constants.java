29a2f4b87ddc8011c072bafba7f7a04b6f191af1
TITANXP <1619450746@qq.com>
2021-09-14 18:37:04 +0800

    添加常量

--- src/main/java/tv/bangumi/recsys/Constants.java
+++ src/main/java/tv/bangumi/recsys/Constants.java
@@ -9,15 +9,41 @@ public interface Constants {
     int REDIS_PORT = 6379;
 
     String TAG_PREFIX = "tag:";
-    String ANIME_PREFIX = "anime:";
-    String USER_PREFIX = "user:";
+    String ANIME_PREFIX = "animeF:";
+    String USER_PREFIX = "userF:";
+    String ANIME_EMB_PREFIX = "animeEmb:";
+    String USER_EMB_PREFIX = "userEmb:";
+    String USER_REC_PREFIX = "userRec:";
+    int USER_REC_TTL = 300;
+    String SIM_ANIME_PREFIX = "simAnime:";
+    int SIM_ANIME_TTL = 30000;
+    String SIM_USER_PREFIX = "simUser:";
+    int SIM_USER_TTL = 300;
 
 
     // MongoDB
     String MONGO_URL = "mongodb://localhost:27017/bangumi";
-    String MONGO_ANIME_COLLECTION = "anime";
-    String MONGO_USER_COLLECTION = "user";
+    String MONGO_HOST = "localhost";
+    int MONGO_PORT = 27017;
+    String MONGO_DB = "bangumi_test";
+    String MONGO_ANIME_COLLECTION = "anime1";
+    String MONGO_USER_COLLECTION = "user1";
+
+    // ElasticSearch
+    String ES_HOST = "localhost";
+    int ES_PORT = 9200;
+    String ES_SCHEME = "http";
+    String ES_ANIME_INDEX = "bangumi_anime";
+    String ES_ANIME_TYPE = "_doc";
 
     // TenserFlow Serving
     String TF_SERVING_REC_MODEL_URL = "http://localhost:8501/v1/models/recmodel:predict";
+    // MODEL
+    int RECALL_SIZE = 300; // 召回层返回的候选物品数
+
+
+
+    int USER_NUM = 583442;
+
+    boolean IS_FILTER = true;
 }
